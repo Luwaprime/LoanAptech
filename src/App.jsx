769927138b1981062a58a3621ec61af075452404
Luwaprime <walecole67@gmail.com>
2026-04-17 11:16:49 +0100

    other comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Terms from './components/Terms';
 
 export default function App() {
   return (
-    <div>
-      <About />
-      <Footer />
+    <Router>
+    <div className='App'>
+      <Routes>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/terms" element={<Terms/>}/>
+      </Routes>
+
+      <Footer/>
     </div>
+    </Router>  
   );
 }
