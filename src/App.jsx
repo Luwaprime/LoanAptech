b4f6eedf57b5ad5aed200bbe445fcf86f9aa1108
Luwaprime <walecole67@gmail.com>
2026-04-27 12:24:45 +0100

    others

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,23 @@ import Footer from './components/Footer';
 import Contact from './components/Contact';
 import Terms from './components/Terms';
 import Faq from './components/Faq';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Signup from './components/signup';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
 
 export default function App() {
   return (
     <Router>
     <div className='App'>
+      <Navbar/>
       <Routes>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path='/' element={<Home />}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/contact" element={<Contact/>}/>
         <Route path="/terms" element={<Terms/>}/>
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Dashboard.css';
+
+export default function Dashborad () {
+    return(
+        <div className='dashboard-page'>
+            <h1>LoanAptech Dashboard</h1>
+        </div>
+            
+    );
+}
